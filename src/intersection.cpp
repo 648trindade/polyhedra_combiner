584effeb0afe33094bef9a5a3bdd425b6199c991
Rafael Gauna Trindade <rtrindade@esss.co>
2023-12-11 19:12:57 -0300

    WIP

--- src/intersection.cpp
+++ src/intersection.cpp
@@ -10,13 +10,11 @@ void intersect(Solid& home_solid, Solid& near_solid)
         return;
     }
 
-    std::vector<size_t> last_near_face(home_solid.faces.size(), 0);
     for (size_t home_face_id = 0; home_face_id < home_solid.faces.size(); home_face_id++)
     {
         const BoundingBox home_face_box = home_solid.faces[home_face_id].bounding_box();
-        const size_t first_near_face = last_near_face[home_face_id];
         const size_t n_near_faces = near_solid.faces.size();
-        for (size_t near_face_id = first_near_face; near_face_id < n_near_faces; near_face_id++)
+        for (size_t near_face_id = 0; near_face_id < n_near_faces; near_face_id++)
         {
             Face& home_face = home_solid.faces[home_face_id];
             Face& near_face = near_solid.faces[near_face_id];
@@ -40,7 +38,6 @@ void intersect(Solid& home_solid, Solid& near_solid)
                     if (new_home_face.get_number_of_vertices() > 0)
                     {
                         home_solid.faces.emplace_back(std::move(new_home_face));
-                        last_near_face.push_back(near_face_id);
                         home_solid.to_OBJ("/tmp/a_" + std::to_string(home_face_id) + "_" + std::to_string(near_face_id) + ".obj");
                     }
                     if (new_near_face.get_number_of_vertices() > 0)
